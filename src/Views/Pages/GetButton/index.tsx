4d6049881e21149727d0ebca724cde6fa8cdf92e
Fatemeh Shahali <49586768+F-Shahali@users.noreply.github.com>
2024-04-14 19:43:21 +0330

    Feature/services_support (#7)
    * add support for share a link to services
    
    * restart shareModal input validation after closing
    
    * show the buttons code added
    
    * copy code to clipboard added
    
    * Get code updated to show error tooltip
    
    * show the result of get buttons code added
    
    * OSL favicon added as website icon
    
    * change `dev` to `main` in buttons icon url
    
    * change `useAdvancedState` to `useData`

--- src/Views/Pages/GetButton/index.tsx
+++ src/Views/Pages/GetButton/index.tsx
@@ -1,14 +1,29 @@
-import EditableInput from '@src/Components/EditableInput/EditableInput';
 import './index.scss';
-import { Button, Col, Modal, Row } from 'rsuite';
-import Email from '@assets/icons/services/email.svg';
-import Gmail from '@assets/icons/services/gmail.svg';
-import Telegram from '@assets/icons/services/telegram.svg';
 import { useState } from 'react';
 import Service from '@src/Components/Service';
+import { Button, Col, Modal, Row, Tooltip, Whisper } from 'rsuite';
+import Email from '@assets/icons/services/email.svg';
+import Gmail from '@assets/icons/services/gmail.svg';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import Telegram from '@assets/icons/services/telegram.svg';
+import EditableInput from '@src/Components/EditableInput/EditableInput';
+import { lightfair } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import FaIcon from '@src/Components/FaIcon';
+import { copyToClipboard } from '@src/Tools/Utils/React';
+import useWindow from '@src/Tools/Hooks/useWindow';
+import { useData } from '@src/Tools/Hooks/useData';
 
 const GetButton = () => {
-	const [open, setOpen] = useState(false);
+	const { isMobile } = useWindow();
+	const [buttons, setButtons] = useState<JSX.Element>();
+	const { set, temp } = useData({
+		url: '',
+		code: '',
+		isValid: true,
+		showCode: false,
+		openModal: false,
+		openTooltip: false,
+	});
 	const [selectedServices, setSelectedServices] = useState<string[]>(['email']);
 
 	// ? -------------------------- Functions ------------------------------
@@ -21,28 +36,155 @@ const GetButton = () => {
 		setSelectedServices(filtered);
 	};
 
+	const getCode = () => {
+		if (!temp.url) {
+			set.ou.temp('isValid', false);
+			return;
+		}
+		if (selectedServices.length === 0) {
+			set.ou.temp('showCode', false);
+			return;
+		}
+
+		const selected = Services(temp.url).filter(service => selectedServices.includes(service.title));
+		const buttons = (
+			<div className='flex-center'>
+				{selected.map((service, i) => {
+					return (
+						<a href={service.url} target='_blank' rel='noreferrer' key={i}>
+							<img
+								src={service.icon}
+								width={32}
+								height={32}
+								style={{ backgroundColor: service.bg, borderRadius: 4, paddingLeft: 1, paddingRight: 1 }}
+								alt=''
+							/>
+						</a>
+					);
+				})}
+			</div>
+		);
+		setButtons(buttons);
+
+		const code = `	<div>
+			${selected
+				.map(service => {
+					return `<a href="${service.url}" target="_blank"><img src="${service.iconUrl}" width="32" height="32" style="background-color:${service.bg}; border-radius:4px"/></a>`;
+				})
+				.join(`\n			`)}
+	</div>`;
+		set.ou.temp('code', code);
+
+		set.ou.temp('showCode', true);
+	};
+
+	const finalUrl = (url: string) => {
+		if (url.startsWith('https://') || url.startsWith('http://')) return url;
+
+		return `http://${url}`;
+	};
+
+	// ? ---------------------- Var -------------------------------
+	const Services = (url?: string) => [
+		{
+			title: 'email',
+			icon: Email,
+			iconUrl: 'https://github.com/openscilab/mybutton/raw/main/src/Assets/icons/services/email.svg',
+			bg: '#888990',
+			url: `mailto:?subject=&body=${finalUrl(url || '')}`,
+		},
+		{
+			title: 'gmail',
+			icon: Gmail,
+			iconUrl: 'https://github.com/openscilab/mybutton/raw/main/src/Assets/icons/services/gmail.svg',
+			bg: '#EA4335',
+			url: `https://mail.google.com/mail/u/0/?ui=2&fs=1&tf=cm&su&body=${finalUrl(url || '')}`,
+		},
+		{
+			title: 'telegram',
+			icon: Telegram,
+			iconUrl: 'https://github.com/openscilab/mybutton/raw/main/src/Assets/icons/services/telegram.svg',
+			bg: '#2CA5E0',
+			url: `https://telegram.me/share/url?url=${finalUrl(url || '')}&text=`,
+		},
+	];
+
 	// ---------------------------------------------------------------------
 	return (
 		<div className='get-button-layout'>
 			<div className='get-button-container'>
 				<h1>Get share button code</h1>
 				<div className='input-container'>
-					<label className='input-label'>Page url</label>
-					<EditableInput label='required' />
+					<EditableInput
+						label='Page URL'
+						defaultValue={temp.url}
+						isValid={temp.isValid}
+						errorMessage='required'
+						onChange={e => {
+							if (!temp.isValid) {
+								console.log(temp);
+								set.ou.temp('isValid', true);
+							}
+							set.ou.temp('url', e.target.value);
+						}}
+						placeholder='https://www.example.com'
+					/>
 				</div>
 				<div className='buttons'>
-					<Button className='choose' onClick={() => setOpen(true)}>
+					<Button className='choose' onClick={() => set.ou.temp('openModal', true)}>
 						Choose Services
 					</Button>
-					<Button className='get-code'>Get Code</Button>
+					<Whisper
+						disabled={selectedServices.length !== 0}
+						placement={isMobile ? 'bottom' : 'top'}
+						trigger='click'
+						speaker={<Tooltip className='err-tooltip'>Please choose services!</Tooltip>}>
+						<Button className='get-code' onClick={getCode}>
+							Get Code
+						</Button>
+					</Whisper>
 				</div>
+				{temp.showCode && (
+					<div className='code-container'>
+						<SyntaxHighlighter language={'xml'} style={lightfair} children={temp.code} />
+						<Whisper
+							className='copy-whisper'
+							onClick={() => {
+								if (!temp.openTooltip) set.ou.temp('openTooltip', true);
+							}}
+							onOpen={() => {
+								setTimeout(() => {
+									set.ou.temp('openTooltip', false);
+								}, 1500);
+							}}
+							open={temp.openTooltip}
+							placement='top'
+							trigger='click'
+							speaker={<Tooltip className='copy-tooltip'>Copied!</Tooltip>}>
+							<div className='copy-icon'>
+								<FaIcon
+									fa='l-clone'
+									onClick={async () => {
+										await copyToClipboard(temp.code);
+									}}
+								/>
+							</div>
+						</Whisper>
+						<div className='services-button'>{buttons}</div>
+					</div>
+				)}
 			</div>
-			<Modal open={open} size='sm' onClose={() => setOpen(false)} backdrop className='choose-services-modal'>
+			<Modal
+				open={temp.openModal}
+				size='sm'
+				onClose={() => set.ou.temp('openModal', false)}
+				backdrop
+				className='choose-services-modal'>
 				<Modal.Header>Choose services</Modal.Header>
 				<Modal.Body>
 					<div className='services-list'>
 						<Row>
-							{Services.map((service, i) => {
+							{Services().map((service, i) => {
 								const checked = selectedServices.includes(service.title);
 								return (
 									<Col xs={12} sm={8} key={i}>
@@ -59,17 +201,11 @@ const GetButton = () => {
 					</div>
 				</Modal.Body>
 				<Modal.Footer>
-					<Button onClick={() => setOpen(false)}>Done</Button>
+					<Button onClick={() => set.ou.temp('openModal', false)}>Done</Button>
 				</Modal.Footer>
 			</Modal>
 		</div>
 	);
 };
-
-const Services = [
-	{ title: 'email', icon: Email, bg: '#888990' },
-	{ title: 'gmail', icon: Gmail, bg: '#EA4335' },
-	{ title: 'telegram', icon: Telegram, bg: '#2CA5E0' },
-];
 
 export default GetButton;
