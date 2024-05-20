489a51428b5a54b2c8811fb771607bce21da73b6
Fatemeh sh <fa.sh9877@gmail.com>
2024-05-20 11:02:16 +0330

    update config file

--- src/App/Config/constants.ts
+++ src/App/Config/constants.ts
@@ -5,28 +5,14 @@ const BaseConfig = {
 	SERVER: '<SERVER_ADDRESS>',
 	APP_NAME: '<APP_NAME>',
 	APP_SHORT_NAME: '<APP_SHORT_NAME>',
-
-	//* Parse
-	Parse: {
-		appId: '<appID>',
-		//exp: 'myAppId',
-
-		serverURL: '<serverURL>',
-		//exp: 'https://back.test.com/parse'
-
-		liveQueryServerURL: '<liveQueryServerURL>',
-		//exp: 'wss://back.test.com'
-	},
 };
 
 const DevConfig = {
-	// SERVER: 'http://127.0.0.1:1337',
-	// API_SERVER: `${BaseConfig.SERVER}/api`,
-	// API_SERVER: 'http://127.0.0.1:1337/api',
+	FRONT_DOMAIN: 'https://www.dev.mybutton.click',
 };
 
 const ProdConfig = {
-	API_SERVER: `${BaseConfig.SERVER}/api`,
+	FRONT_DOMAIN: 'https://www.mybutton.click',
 };
 
 export const CONFIG = { ...BaseConfig, ...(isDev ? DevConfig : ProdConfig) };
