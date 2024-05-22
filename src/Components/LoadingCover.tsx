d6d29393248866babef6b39c536b2b389a0e83c2
Fatemeh sh <fa.sh9877@gmail.com>
2024-05-22 20:59:24 +0330

    LoadingCover loader icon updated

--- src/Components/LoadingCover.tsx
+++ src/Components/LoadingCover.tsx
@@ -1,9 +1,11 @@
-import { Loader } from 'rsuite';
+import { ReactComponent as Loader } from '@assets/icons/circles.svg';
 
 type LoadingCoverProps = { className?: string };
 
 const LoadingCover: FC<LoadingCoverProps> = ({ className = '' }) => (
-	<Loader className={'loading-cover ' + className} speed='slow' size='lg' center />
+	<div className={'loading-cover ' + className}>
+		<Loader className={'loading-cover-loader ' + className} />
+	</div>
 );
 
 export default LoadingCover;
