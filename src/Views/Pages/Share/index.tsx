cccadb3cadec0e697613940dc91c783318f33b11
Fatemeh sh <fa.sh9877@gmail.com>
2024-05-22 21:18:25 +0330

    share route added for indirect sharing

---
+++ src/Views/Pages/Share/index.tsx
@@ -0,0 +1,36 @@
+import './index.scss';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import LoadingCover from '@src/Components/LoadingCover';
+
+const Share = () => {
+	const location = useLocation();
+
+	const services_url = (subject: string, link: string): { [key: string]: string } => {
+		return {
+			email: `mailto:?subject=${subject}&body=${link}`,
+			gmail: `https://mail.google.com/mail/u/0/?ui=2&fs=1&tf=cm&su=${subject}&body=${link}`,
+			telegram: `https://telegram.me/share/url?url=${link}&text=${subject}`,
+		};
+	};
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search);
+		const service = urlParams.get('service') || '';
+		const subject = urlParams.get('subject') || '';
+		const link = urlParams.get('link') || '';
+
+		const url = services_url(subject, link)[service];
+		setTimeout(() => {
+			window.open(url, '_self');
+		}, 100);
+	}, [location.search]);
+
+	return (
+		<div className='share-layout'>
+			<LoadingCover />
+		</div>
+	);
+};
+
+export default Share;
