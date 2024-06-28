ec7376bee8575a5ec33618d54c55c945c4cf6e20
Fatemeh Shahali <49586768+F-Shahali@users.noreply.github.com>
2024-06-28 19:48:28 +0330

    add MyButton version to the footer (#22)

--- src/Views/Layout/Footer/index.tsx
+++ src/Views/Layout/Footer/index.tsx
@@ -1,21 +1,25 @@
 import './index.scss';
 import { ReactComponent as Github } from '@assets/icons/github.svg';
 import { ReactComponent as Discord } from '@assets/icons/discord.svg';
+import { CONFIG } from '@src/App/Config/constants';
 
 const Footer = () => {
 	return (
 		<div className='footer-layout'>
-			<a href='https://openscilab.com' target='_blank' rel='noreferrer' className='empowered'>
-				Empowered by OSL team
-			</a>
-			<div className='social-links'>
-				<a href='https://discord.gg/T2VzhzqU67' target='_blank' rel='noreferrer'>
-					<Discord className='social-icon' />
+			<div className='about-mybutton'>
+				<a href='https://openscilab.com' target='_blank' rel='noreferrer' className='empowered'>
+					Empowered by OSL team
 				</a>
-				<a href='https://github.com/openscilab/mybutton' target='_blank' rel='noreferrer'>
-					<Github className='social-icon' />
-				</a>
+				<div className='social-links'>
+					<a href='https://discord.gg/T2VzhzqU67' target='_blank' rel='noreferrer'>
+						<Discord className='social-icon' />
+					</a>
+					<a href='https://github.com/openscilab/mybutton' target='_blank' rel='noreferrer'>
+						<Github className='social-icon' />
+					</a>
+				</div>
 			</div>
+			<div className='version'>Version {CONFIG.VERSION}</div>
 		</div>
 	);
 };
