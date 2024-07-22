1f5096f3fc2c12d29d59ffa2fb4117c049d279b8
Fatemeh Shahali <49586768+F-Shahali@users.noreply.github.com>
2024-07-22 17:30:26 +0330

    Remove unused files (#24)
    * unused files & parameters removed
    
    * unused modules removed in `package.json`
    
    * `global.d.ts` file updated

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,17 +4,8 @@ import '@assets/scss/base/RSuite.scss';
 import '@assets/scss/App.scss';
 import '@assets/scss/base/tailwind.scss';
 import RoutesRenderer from './Routes/RouteRenderer';
-import { useEffect } from 'react';
-import { isDev } from '../Tools/Utils/React';
 
 const App = () => {
-	//? ------------------------------ useEffects ------------------------------------------------------
-
-	useEffect(() => {
-		isDev && (window.logs = true);
-	}, []);
-
-	// -------------------------------------------------------------------------------------------------
 	return <RoutesRenderer />;
 };
 
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,25 +1,6 @@
 interface Window {
-	logs?: boolean;
-	user_token?: string;
 	CONFIG?: OBJECT;
 	MODE?: 'PRODUCTION' | 'DEVELOPMENT' | 'LOCAL';
-}
-
-declare module 'react-contextmenu' {
-	interface ContextMenuProps {
-		children?: React.ReactNode;
-	}
-	interface ContextMenuTriggerProps {
-		children?: React.ReactNode;
-	}
-	interface MenuItemProps {
-		children?: React.ReactNode;
-	}
-}
-
-declare module '*.mp4' {
-	const src: string;
-	export default src;
 }
 
 declare module 'react-dom/client';
@@ -28,10 +9,4 @@ type SUG<T extends string = string> = T | (string & {});
 
 type OBJECT<T = any> = Record<string | number | symbol, T>;
 
-type EndsWith<T, U extends string> = T extends `${any}${U}` ? T : U;
-
-type StartsWith<T, U extends string> = T extends `${U}${any}` ? T : U;
-
 type FC<P = {}> = React.FunctionComponent<{ children?: React.ReactNode } & P>;
-
-type Includes<T, U extends string> = T extends `${any}${U}${any}` ? T : U;
