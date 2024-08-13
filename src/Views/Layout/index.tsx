2653bf02e2317d133de7fe2dcabc2712fd308fd4
Fatemeh sh <fa.sh9877@gmail.com>
2024-08-13 23:00:14 +0330

    handle `path` query param and redirect to the relevant route

--- src/Views/Layout/index.tsx
+++ src/Views/Layout/index.tsx
@@ -4,8 +4,28 @@ import ShareModal from './ShareModal';
 import PagesRouter from '../Pages/router';
 import { ReactComponent as Bg } from '@assets/Images/bg.svg';
 import Footer from './Footer';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Layout = () => {
+	const navigate = useNavigate();
+	const params_string = window.location.search;
+	const urlParams = new URLSearchParams(params_string);
+	const path = urlParams.get('path');
+
+	useEffect(() => {
+		if (path) {
+			urlParams.delete('path');
+			const new_params = urlParams.toString();
+			navigate(`/${path}`);
+			if (new_params) window.location.search = new_params;
+		}
+	}, []);
+
+	if (path) {
+		return <div className='root-layout' />;
+	}
+
 	return (
 		<div className='root-layout'>
 			<ShareModal />
