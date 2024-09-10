eecfe5bced4340604709e90f825ab71f0f07bb50
Fatemeh sh <fa.sh9877@gmail.com>
2024-09-10 22:24:01 +0330

    redirect other paths to `/`

--- src/App/Routes/routes.tsx
+++ src/App/Routes/routes.tsx
@@ -1,11 +1,17 @@
 import { lazy } from 'react';
 import { Path } from './RouteRenderer';
+import { Navigate } from 'react-router-dom';
 
 export const routesArray: Path[] = [
 	{
-		path: ['*'],
+		path: ['/'],
 		exact: false,
 		component: lazy(() => import('../../Views/Layout')),
+	},
+	{
+		path: ['*'],
+		exact: true,
+		component: () => <Navigate to={'/'} />,
 	},
 ];
 
