5d2b697dc1d6e32651f77d17a8a3feaf2254a1a9
Fatemeh sh <fa.sh9877@gmail.com>
2024-09-10 16:30:29 +0330

    Redux structure updated

--- src/Tools/Store/slices/index.ts
+++ src/Tools/Store/slices/index.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import LocalCacheReducer from './LocalCacheReducer';
+import LocalCacheReducer from './LocalCacheSlice';
 import { CONFIG } from './../../../App/Config/constants';
 import createCompressor from 'redux-persist-transform-compress';
 
@@ -13,10 +13,10 @@ const reducers = combineReducers({
 			storage,
 			key: 'storage',
 			keyPrefix: `${CONFIG.APP_SHORT_NAME}-`,
-			blacklist: ['openShareModal'],
+			blacklist: ['openShareModal', 'activePage'],
 			transforms: [compressor] as any,
 		},
-		LocalCacheReducer
+		LocalCacheReducer.reducer
 	),
 });
 
