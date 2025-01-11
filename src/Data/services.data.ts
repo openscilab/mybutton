1823afd7f21f335a409a04e488c34681ae41ba7d
Fatemeh sh <fa.sh9877@gmail.com>
2025-01-11 23:26:35 +0330

    add `Blogger` & `Pinterest` & `Reddit` & `Trello`

--- src/Data/services.data.ts
+++ src/Data/services.data.ts
@@ -1,12 +1,16 @@
+import X from '@assets/icons/services/x.svg';
 import Email from '@assets/icons/services/email.svg';
 import Gmail from '@assets/icons/services/gmail.svg';
+import Yahoo from '@assets/icons/services/yahoo.svg';
+import Reddit from '@assets/icons/services/reddit.svg';
+import Trello from '@assets/icons/services/trello.svg';
+import Blogger from '@assets/icons/services/blogger.svg';
 import Telegram from '@assets/icons/services/telegram.svg';
 import Whatsapp from '@assets/icons/services/whatsapp.svg';
-import Yahoo from '@assets/icons/services/yahoo.svg';
 import Facebook from '@assets/icons/services/facebook.svg';
 import Linkedin from '@assets/icons/services/linkedin.svg';
+import Pinterest from '@assets/icons/services/pinterest.svg';
 import HackerNews from '@assets/icons/services/hacker-news.svg';
-import X from '@assets/icons/services/x.svg';
 
 export const services_url = (url: string, subject?: string): { [key: string]: string } => {
 	return {
@@ -19,6 +23,10 @@ export const services_url = (url: string, subject?: string): { [key: string]: st
 		'Hacker News': `https://news.ycombinator.com/submitlink?u=${url}&t=${subject}`,
 		Facebook: `https://www.facebook.com/sharer/sharer.php?u=${url}&t=${subject}`,
 		LinkedIn: `https://www.linkedin.com/shareArticle?mini=true&url=${url}&text=${subject}`,
+		Reddit: `https://www.reddit.com/submit?url=${url}&title=${subject}`,
+		Pinterest: `https://www.pinterest.com/pin/create/button/?url=${url}&media=&description=${subject}`,
+		Blogger: `https://www.blogger.com/blog_this.pyra?u=${url}&n=${subject}&t&pli=1`,
+		Trello: `https://trello.com/add-card?name=${subject}&desc=${url}`,
 	};
 };
 
@@ -76,5 +84,29 @@ export const Services = [
 		icon: Linkedin,
 		iconUrl: 'https://github.com/openscilab/mybutton/raw/main/src/Assets/icons/services/linkedin.svg',
 		bg: '#007bb5',
+	},
+	{
+		title: 'Reddit',
+		icon: Reddit,
+		iconUrl: 'https://github.com/openscilab/mybutton/raw/main/src/Assets/icons/services/reddit.svg',
+		bg: '#ff4500',
+	},
+	{
+		title: 'Pinterest',
+		icon: Pinterest,
+		iconUrl: 'https://github.com/openscilab/mybutton/raw/main/src/Assets/icons/services/pinterest.svg',
+		bg: '#e60023',
+	},
+	{
+		title: 'Blogger',
+		icon: Blogger,
+		iconUrl: 'https://github.com/openscilab/mybutton/raw/main/src/Assets/icons/services/blogger.svg',
+		bg: '#fda352',
+	},
+	{
+		title: 'Trello',
+		icon: Trello,
+		iconUrl: 'https://github.com/openscilab/mybutton/raw/main/src/Assets/icons/services/trello.svg',
+		bg: '#0079bf',
 	},
 ];
